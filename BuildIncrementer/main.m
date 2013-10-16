19a7b0e084f9c62fd06d09d67a4e0143284c9113
Dima Bart <dbart@Dimas-Mac-Mini.local>
2013-10-16 13:31:24 -0400

    Initial Commit

---
+++ BuildIncrementer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  BuildIncrementer
+//
+//  Created by Dima Bart on 2013-10-16.
+//  Copyright (c) 2013 Dima Bart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
