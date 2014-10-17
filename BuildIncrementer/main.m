9d61e769a0283a914c4de9e80b4a758417029b16
Dima Bart <dbart@Dimas-Mac-Mini.local>
2014-10-17 10:29:10 -0400

    - updated for readability - various cleanup

--- BuildIncrementer/main.m
+++ BuildIncrementer/main.m
@@ -8,10 +8,51 @@
 
 #import <Foundation/Foundation.h>
 
-#pragma mark - Function Prototypes -
-id plistAtFilepath(NSString *filepath);
-BOOL writePlistToFilepath(id plist, NSString *filepath);
-NSString *incrementBuildString(NSString *buildString);
+#pragma mark - Functions -
+static id BIPlistAtFilepath(NSString *filepath) {
+    NSError *error    = nil;
+    NSData *plistData =  [NSData dataWithContentsOfFile:filepath options:0 error:&error];
+    if (!plistData) {
+        NSLog(@"Error reading plist: %@", error.localizedDescription);
+        return nil;
+    }
+    
+    NSError *plistError = nil;
+    id plist = [NSPropertyListSerialization propertyListWithData:plistData options:NSPropertyListMutableContainersAndLeaves format:NULL error:&plistError];
+    if (!plist) {
+        NSLog(@"Error reading plist: %@", plistError.localizedDescription);
+    }
+    
+    return plist;
+}
+
+static BOOL BIWritePlistToFilepath(id plist, NSString *filepath) {
+    NSError *error    = nil;
+    NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:plist format:NSPropertyListXMLFormat_v1_0 options:0 error:&error];
+    if (!plistData) {
+        NSLog(@"Error creating plist: %@", error.localizedDescription);
+        return NO;
+    }
+    
+    NSError *writeError = nil;
+    if (![plistData writeToFile:filepath options:NSDataWritingAtomic error:&writeError]) {
+        NSLog(@"Error writing plist: %@", writeError.localizedDescription);
+        return NO;
+    }
+    
+    return YES;
+}
+
+static NSString * BIIncrementBuildString(NSString *buildString) {
+    NSMutableArray *numberComponents = [[buildString componentsSeparatedByString:@"."] mutableCopy];
+    NSUInteger components            = [numberComponents count];
+    
+    int buildNumber                  = [[numberComponents lastObject] intValue];
+    NSString *newBuildNumber         = [NSString stringWithFormat:@"%i", ++buildNumber];
+    
+    [numberComponents replaceObjectAtIndex:components-1 withObject:newBuildNumber];
+    return [numberComponents componentsJoinedByString:@"."];
+}
 
 #pragma mark - Main -
 int main(int argc, const char * argv[]) {
@@ -21,136 +62,20 @@ int main(int argc, const char * argv[]) {
         const char *filestring = argv[1];
         
         NSString *filePath = [NSString stringWithUTF8String:filestring];
-        NSMutableDictionary *plist = plistAtFilepath(filePath);
+        NSMutableDictionary *plist = BIPlistAtFilepath(filePath);
         if (!plist) {
             return 1;
         }
         
         NSString *currentBuild = [plist objectForKey:(NSString *)kCFBundleVersionKey];
-        NSString *newBuild     = incrementBuildString(currentBuild);
+        NSString *newBuild     = BIIncrementBuildString(currentBuild);
         
         [plist setObject:newBuild forKey:(NSString *)kCFBundleVersionKey];
         
-        if (!writePlistToFilepath(plist, filePath)) {
-            NSLog(@"Failure.");
+        if (!BIWritePlistToFilepath(plist, filePath)) {
+            NSLog(@"Failed to increment project property list at path: %s", filestring);
             return 1;
-        } else {
-            NSLog(@"Success.");
         }
     }
     return 0;
 }
-
-#pragma mark - Functions -
-id plistAtFilepath(NSString *filepath) {
-    NSLog(@"Reading plist...");
-    
-    NSError *error = nil;
-    NSData *plistData =  [NSData dataWithContentsOfFile:filepath options:0 error:&error];
-    if (!plistData) {
-        NSLog(@"Error reading plist: %@",error.localizedDescription);
-        return nil;
-    }
-    
-    NSError *plistError = nil;
-    id plist = [NSPropertyListSerialization propertyListWithData:plistData options:NSPropertyListMutableContainersAndLeaves format:NULL error:&plistError];
-    if (!plist) {
-        NSLog(@"Error reading plist: %@",plistError.localizedDescription);
-        return nil;
-    }
-    
-    return plist;
-}
-
-BOOL writePlistToFilepath(id plist, NSString *filepath) {
-    NSLog(@"Writing plist...");
-    
-    NSError *error = nil;
-    NSData *plistData = [NSPropertyListSerialization dataWithPropertyList:plist format:NSPropertyListXMLFormat_v1_0 options:0 error:&error];
-    if (!plistData) {
-        NSLog(@"Error creating plist: %@",error.localizedDescription);
-        return NO;
-    }
-    
-    NSError *writeError = nil;
-    if (![plistData writeToFile:filepath options:NSDataWritingAtomic error:&writeError]) {
-        NSLog(@"Error writing plist: %@",writeError.localizedDescription);
-        return NO;
-    }
-    
-    return YES;
-}
-
-NSString *incrementBuildString(NSString *buildString) {
-    NSLog(@"Current build version: %@",buildString);
-    
-    NSMutableArray *numberComponents = [[buildString componentsSeparatedByString:@"."] mutableCopy];
-    NSUInteger components = [numberComponents count];
-    
-    int number = [[numberComponents lastObject] intValue];
-    NSString *newBuildNumber = [NSString stringWithFormat:@"%i",++number];
-    [numberComponents replaceObjectAtIndex:components-1 withObject:newBuildNumber];
-    
-    NSString *newBuild = [numberComponents componentsJoinedByString:@"."];
-    NSLog(@"New build version:     %@",newBuild);
-    
-    return newBuild;
-}
-
-#pragma mark - Regex Version -
-//        // Regex version
-//        NSLog(@"Opening file: %@",filePath);
-//
-//        NSError *error = nil;
-//        NSMutableString *fileContents = [NSMutableString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
-//        if (!fileContents) {
-//            NSLog(@"Error, no file contents: %@",error.localizedDescription);
-//            return 1;
-//        } else {
-//            NSLog(@"Reading file...");
-//        }
-//
-//        NSError *regexError = nil;
-//        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"<key>CFBundleVersion</key>.+?<string>(.+?)</string>" options:NSRegularExpressionDotMatchesLineSeparators error:&regexError];
-//        if (!regex) {
-//            NSLog(@"Error: %@",regexError.localizedDescription);
-//            return 1;
-//        } else {
-//            NSLog(@"Parsing regex...");
-//        }
-//
-//        [regex enumerateMatchesInString:fileContents options:0 range:NSMakeRange(0, fileContents.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-//            NSUInteger captures = [result numberOfRanges];
-//            if (captures > 0) {
-//                NSRange captureRange = [result rangeAtIndex:1];
-//                NSString *buildNumber = [fileContents substringWithRange:captureRange];
-//                NSLog(@"Current build version: %@",buildNumber);
-//
-//                NSMutableArray *numberComponents = [[buildNumber componentsSeparatedByString:@"."] mutableCopy];
-//                NSUInteger components = [numberComponents count];
-//
-//                int number = [[numberComponents lastObject] intValue];
-//                NSString *newBuildNumber = [NSString stringWithFormat:@"%i",++number];
-//                [numberComponents replaceObjectAtIndex:components-1 withObject:newBuildNumber];
-//
-//                NSString *finalVersionString = [numberComponents componentsJoinedByString:@"."];
-//                NSLog(@"New build version:     %@",finalVersionString);
-//                [fileContents replaceCharactersInRange:captureRange withString:finalVersionString];
-//
-//                NSLog(@"Incremented version number.");
-//                NSLog(@"Writing file...");
-//
-//                NSError *error = nil;
-//                if (![fileContents writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error]) {
-//                    NSLog(@"Failed to write file: %@",error.localizedDescription);
-//                    return;
-//                } else {
-//                    NSLog(@"Success!");
-//                    return;
-//                }
-//            } else {
-//                NSLog(@"No regex captures. Increment failed.");
-//                return;
-//            }
-//        }];
-
